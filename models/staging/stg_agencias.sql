e14d2d065dcffa25347ebe6f94432569e3b2d29d
bortoliluiza@gmail.com <bortoliluiza@gmail.com>
2025-09-02 22:54:19 +0000

    added stg_agencias.sql and tests for this model

--- models/staging/stg_agencias.sql
+++ models/staging/stg_agencias.sql
@@ -1,1 +1,17 @@
-select * from {{ source('raw','agencias') }}
+with src as (
+
+    select
+        cod_agencia,
+        nome,
+        endereco,
+        cidade,
+        uf,
+        data_abertura,
+        tipo_agencia
+
+    from {{ source('raw', 'agencias') }}
+
+)
+
+
+select * from src
