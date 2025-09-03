f4c38fdac10f091ef32d74291e5a095ef7b52002
bortoliluiza@gmail.com <bortoliluiza@gmail.com>
2025-09-03 01:34:18 +0000

    added stg_colaboradores and tests

--- models/staging/stg_colaboradores.sql
+++ models/staging/stg_colaboradores.sql
@@ -1,1 +1,18 @@
-select * from {{ source('raw','colaboradores') }}
+with src as (
+
+    select
+        cod_colaborador,
+        primeiro_nome,
+        ultimo_nome,
+        email,
+        regexp_replace(cpf, r'[^0-9]', '') as cpf,
+        data_nascimento,
+        endereco,
+        regexp_replace(cep, r'[^0-9]', '') as cep     
+                
+    from {{ source('raw', 'colaboradores') }}
+
+)
+
+
+select * from src
