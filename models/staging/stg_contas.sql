eb22f79058d875d689f38d22f1d841257483df38
bortoliluiza@gmail.com <bortoliluiza@gmail.com>
2025-09-03 02:08:50 +0000

    created stg_contas.sql and tests

--- models/staging/stg_contas.sql
+++ models/staging/stg_contas.sql
@@ -1,1 +1,19 @@
-select * from {{ source('raw','contas') }}
+with src as (
+
+    select
+        num_conta,
+        cod_cliente,
+        cod_agencia,
+        cod_colaborador,
+        tipo_conta,
+        data_abertura,
+        cast(saldo_total as numeric) as saldo_total,
+        cast(saldo_disponivel as numeric) as saldo_disponivel,
+        data_ultimo_lancamento        
+                
+    from {{ source('raw', 'contas') }}
+
+)
+
+
+select * from src
