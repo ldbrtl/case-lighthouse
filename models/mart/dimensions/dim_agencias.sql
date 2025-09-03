b142475b6bde4acf66b89509af382324e1362694
bortoliluiza@gmail.com <bortoliluiza@gmail.com>
2025-09-03 16:13:40 +0000

    created dim_agencias.sql to visualize the ranking later

---
+++ models/mart/dimensions/dim_agencias.sql
@@ -0,0 +1,17 @@
+with src as (
+    select
+    cod_agencia,
+    nome,
+    tipo_agencia,
+    endereco,
+    cidade,
+    uf,
+    data_abertura
+
+    from {{ ref('stg_agencias') }}
+
+
+)
+
+select * from src
+
