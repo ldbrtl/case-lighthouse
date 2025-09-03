411218da3ca1d7f460b5e2c83b74b0113a8701ed
bortoliluiza@gmail.com <bortoliluiza@gmail.com>
2025-09-03 02:51:31 +0000

    created stg_propostas_credito and tests

--- models/staging/stg_propostas_credito.sql
+++ models/staging/stg_propostas_credito.sql
@@ -1,1 +1,22 @@
-select * from {{ source('raw','propostas_credito') }}
+with src as (
+
+    select
+        cod_proposta,
+        cod_cliente,
+        cod_colaborador,
+        data_entrada_proposta,
+        cast(taxa_juros_mensal as numeric) as taxa_juros_mensal,
+        cast(valor_proposta as numeric) as valor_proposta,
+        cast(valor_financiamento as numeric) as valor_financiamento,
+        cast(valor_entrada as numeric) as valor_entrada,
+        cast(valor_prestacao as numeric) as valor_prestacao,
+        quantidade_parcelas,
+        carencia,
+        status_proposta              
+                
+    from {{ source('raw', 'propostas_credito') }}
+
+)
+
+
+select * from src
