399abcbb8ce56b4aaf0c94b2c277c9fc163d588e
bortoliluiza@gmail.com <bortoliluiza@gmail.com>
2025-09-03 03:01:49 +0000

    created stg_transacoes and tests

--- models/staging/stg_transacoes.sql
+++ models/staging/stg_transacoes.sql
@@ -1,1 +1,15 @@
-select * from {{ source('raw','transacoes') }}
+with src as (
+
+    select
+        cod_transacao,
+        num_conta,
+        data_transacao,
+        nome_transacao,
+        cast(valor_transacao as numeric) as valor_transacao          
+                
+    from {{ source('raw', 'transacoes') }}
+
+)
+
+
+select * from src
